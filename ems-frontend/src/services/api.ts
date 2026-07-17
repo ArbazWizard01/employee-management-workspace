09be63181f67e56805467b7e0d8f25cad8b7aee9
Arbaz Ansari <arbazansari0113@gmail.com>
2026-07-18 03:21:52 +0530

    fix: update backend subdomain to ui4qtbc17

--- ems-frontend/src/services/api.ts
+++ ems-frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://playstackemsbackend-mzucorob.b4a.run/api",
+  baseURL: "https://playstackemsbackend-u46bbc17.b4a.run/api",
 });
 
 api.interceptors.request.use((config) => {
